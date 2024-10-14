649af0f8aa658e978c04cd822a0cf308f7c09077
Wyatt St. Onge <142924797+SaintOngeJ@users.noreply.github.com>
2024-10-14 16:43:58 -0500

    Added Tetraminoes

--- tetrisGameCS250GroupProject/src/tetrisGameCS250GroupProject/TetrisGame.java
+++ tetrisGameCS250GroupProject/src/tetrisGameCS250GroupProject/TetrisGame.java
@@ -3,9 +3,7 @@
  * Author: Wyatt St. Onge, Weston Cory, Everett Rosenow
  * Date: Sep 21, 2024
  * Assignment: tetrisGameCS250GroupProject
- * Goals:
- * a)
- * b)
+ * Goals: This class handles all of the tetris game logic
  * Inputs:
  * Outputs:
  * Packages: tetrisGameCS250GroupProject,
@@ -13,7 +11,70 @@
  */
 package tetrisGameCS250GroupProject;
 
+import java.awt.Point;
+
 public class TetrisGame {
+	
+	private final Point[][][] Tetraminoes = {
+		
+		// O-piece
+		{
+			{new Point(0, 0),new Point(0, 1),new Point(1, 0),new Point(1, 1)},
+			{new Point(0, 0),new Point(0, 1),new Point(1, 0),new Point(1, 1)},
+			{new Point(0, 0),new Point(0, 1),new Point(1, 0),new Point(1, 1)},
+			{new Point(0, 0),new Point(0, 1),new Point(1, 0),new Point(1, 1)}
+		},
+		
+		// I-piece
+		{
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(3, 1)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(1, 3)},
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(3, 1)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(1, 3)},
+		},
+		
+		// J-piece
+		{
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(2, 0)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(2, 2)},
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(0, 2)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(0, 0)}
+		},
+		
+		// L-piece
+		{
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(2, 2)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(0, 2)},
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(0, 0)},
+			{new Point(1, 0),new Point(1, 1),new Point(1, 2),new Point(2, 0)}
+		},
+		
+		// S-piece
+		{
+			{new Point(1, 0),new Point(2, 0),new Point(0, 1),new Point(1, 1)},
+			{new Point(0, 0),new Point(0, 1),new Point(1, 1),new Point(1, 2)},
+			{new Point(1, 0),new Point(2, 0),new Point(0, 1),new Point(1, 1)},
+			{new Point(0, 0),new Point(0, 1),new Point(1, 1),new Point(1, 2)}
+		},
+		
+		// T-piece
+		{
+			{new Point(1, 0),new Point(0, 1),new Point(1, 1),new Point(2, 1)},
+			{new Point(1, 0),new Point(0, 1),new Point(1, 1),new Point(1, 2)},
+			{new Point(0, 1),new Point(1, 1),new Point(2, 1),new Point(1, 2)},
+			{new Point(1, 0),new Point(1, 1),new Point(2, 1),new Point(1, 2)}
+		},
+		
+		// Z-piece
+		{
+			{new Point(0, 0),new Point(1, 0),new Point(1, 1),new Point(2, 1)},
+			{new Point(1, 0),new Point(0, 1),new Point(1, 1),new Point(0, 2)},
+			{new Point(0, 0),new Point(1, 0),new Point(1, 1),new Point(2, 1)},
+			{new Point(1, 0),new Point(0, 1),new Point(1, 1),new Point(0, 2)}
+		}
+	};
+	
+	
 
 	/**
 	 * @param args
